0afb91517d5edc4c0bca0881b28bad13e6c7c6d6
Paweł Wilkosz <p.wilkosz@clearcode.cc>
2022-12-22 16:11:50 +0100

    day 18 second star

--- 2022/day18/index.ts
+++ 2022/day18/index.ts
@@ -34,14 +34,61 @@ function* generateTriples(maxX: number, maxY: number, maxZ: number) {
   }
 }
 
-function isAirPocket(cube: Cube, cubes: Cube[]) {
-  const isAlreadyPresent = cubes.find(([x,y,z]) => x === cube[0] && y === cube[1] && z === cube[2])
+function isAirPocket(startingCube: Cube, cubes: Cube[], maxX: number, maxY: number, maxZ: number) {
+  const isAlreadyPresent = cubes.find(([x,y,z]) => x === startingCube[0] && y === startingCube[1] && z === startingCube[2])
 
   if (isAlreadyPresent) return false
 
-  const adjacentCubes = cubes.filter(isCubeAdjacentTo(cube))
+  const pointsToVisit = [startingCube]
+  const alreadyVisited = new Set<string>()
+  const allCubes = new Set(cubes.map(cube => cube.join(',')))
+  let next: Cube|undefined
 
-  return adjacentCubes.length === 6
+  while (next = pointsToVisit.shift()) {
+    const [x, y, z] = next
+
+    if (x === 0 || x === maxX || y === 0 || y === maxY || z === 0 || z === maxZ) {
+      return false
+    }
+
+    const neighbors = [
+      [x+1, y, z],
+      [x-1, y, z],
+      [x, y+1, z],
+      [x, y-1, z],
+      [x, y, z+1],
+      [x, y, z-1],
+    ] as const
+
+    const viableNeighbors = neighbors.filter(n => {
+      const key = n.join(',')
+
+      return !alreadyVisited.has(key) && !allCubes.has(key)
+    })
+
+    viableNeighbors.forEach(neighbor => {
+      alreadyVisited.add(neighbor.join(','))
+      pointsToVisit.push(neighbor)
+    })
+  }
+
+  return true
+}
+
+function eliminateAirPockets(cubes: Cube[]) {
+  const maxX = cubes.map(([x]) => x).reduce(maximum, 0)
+  const maxY = cubes.map(([,y]) => y).reduce(maximum, 0)
+  const maxZ = cubes.map(([,,z]) => z).reduce(maximum, 0)
+
+  for (const cube of generateTriples(maxX, maxY, maxZ)) {
+    if (isAirPocket(cube, cubes, maxX, maxY, maxZ)) cubes.push(cube)
+  }
+}
+
+function countVisibleSidesWithoutAirPockets(cubes: Cube[]) {
+  eliminateAirPockets(cubes)
+
+  return countVisibleSides(cubes)
 }
 
 function parseInput(input: string): Cube[] {
@@ -70,3 +117,6 @@ const cubes = parseInput(readFileSync('./input.txt', {encoding: 'utf-8'}))
 
 console.log(countVisibleSides(testCubes))
 console.log(countVisibleSides(cubes))
+
+console.log(countVisibleSidesWithoutAirPockets(testCubes))
+console.log(countVisibleSidesWithoutAirPockets(cubes))
